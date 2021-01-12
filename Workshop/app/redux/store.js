3d4a648099ce2c6ef80aa1fc0d9dfad1124b532f
Jorge Acosta <jorge.acosta@externos.bci.cl>
2021-01-12 16:02:42 -0300

    Add thunk in Store

--- Workshop/app/redux/store.js
+++ Workshop/app/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
 import reducers from './reducers'
 
 const rootReducer = combineReducers(reducers)
 
 const store = createStore(
   rootReducer,
+  applyMiddleware(thunk)
 )
 
 export default store
